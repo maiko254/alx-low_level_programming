01c510a42da64002d1c29a80fae0e0e23bd06f30
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-11 09:44:08 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ *
+ * Return:0 Always zero
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 9; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 10; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 8 && n2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -17,3 +17,5 @@
 8-print_base16.c - Program that prints all the numbers of base 16 in lowercase, followed by a new line.
 
 9-print_comb.c - Program that prints all possible combinations of single-digit numbers.
+
+100-print_comb3.c - Program that prints all possible different combinations of two digits.
