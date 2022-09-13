e9d01e13c58a94da8398434246dab24c758e4c2d
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-13 04:12:42 -0700

    print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: number to be checked
+ *
+ * Return:1 if greater than zero, 0 if zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -7,3 +7,5 @@
 3-islower.c - Function that checks for lowercase character. 
 
 4-isalpha.c - Function that checks for alphabetic character.
+
+5-sign.c - Function that prints the sign of a number. Returns 1 and prints + if n is greater than zero. Returns 0 and prints 0 if n is zero. Returns -1 and prints - if n is less than zero.
