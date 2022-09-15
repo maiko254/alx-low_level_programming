ee46effe1449e0ad546c9a337e74ea07c238795f
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-15 08:26:20 -0700

    print triangle using #

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,13 +7,13 @@
 
 void print_triangle(int size)
 {
-	int i , j;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = size - i; j > 0; j++)
+			for (j = size - i; j > 0; j--)
 				_putchar(' ');
 
 			for (j = 0; j < i; j++)
@@ -21,7 +21,7 @@ void print_triangle(int size)
 
 			if (i == size)
 				continue;
-			
+
 			_putchar('\n');
 		}
 	}
