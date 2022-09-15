f3a1934f1e365a86d74bae1eb1b46218904d557d
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-15 03:14:10 -0700

    check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc - Header file containing declarations for functions
+ * used in this directory
+ */
+
+int _putchar(int c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
