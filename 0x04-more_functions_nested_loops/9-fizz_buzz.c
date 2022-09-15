de80bcb43523048202fd56c7b0e06254950f4c0a
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-15 07:07:41 -0700

    correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,15 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz ");
-		if ((i % 3) == 0)
+		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
+
+		if (i == 100)
+			continue;
 	}
 	printf("\n");
 
