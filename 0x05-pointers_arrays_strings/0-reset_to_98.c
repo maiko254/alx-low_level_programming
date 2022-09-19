49190a7d012a15a20dd807c9712fbd7fa54d1487
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-19 05:06:20 -0700

    update value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value pointed to 98
+ * @n: pointer to integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc - Header file containing declarations for functions
+ * used in this directory
+ */
+
+int _putchar(int c);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
