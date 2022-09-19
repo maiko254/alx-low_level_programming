cab3f15858c6a0986162dbc45d05d245391f5a54
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-19 12:18:53 -0700

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int i = 0;
 
-	while (src[index++])
-		dest[index] = src[index];
+	while (*(src + 1) != 0)
+	{
+		*(dest + 1) = *(src + 1);
+		i++;
+	}
 
 	return (dest);
 }
