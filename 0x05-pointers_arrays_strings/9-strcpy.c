3845e6060da9d3c553901714f9956dd98eda4e8a
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-19 12:39:31 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i++] = '\0';
 
 	return (dest);
 }
