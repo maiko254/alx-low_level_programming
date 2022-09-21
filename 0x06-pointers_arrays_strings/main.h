b784d6a454437c9232eb839f92c7fc6a0e5be8fd
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-21 03:39:10 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc - Header file containing declarations for functions
+ * used in this directory
+ */
+
+int _putchar(int c);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
