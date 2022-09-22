2e9b333d2e2aaa0ef5d26871f2126e0ea265138c
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-22 09:30:47 -0700

    correction to task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,19 +9,22 @@
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	int spc = 13;
-	char sep[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'}
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (s[i])
+	while (*(s + i))
 	{
-		j = 0;
-
-		while (j < spc)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if ((i == 0) || s[i - 1] == sep[i] && (s[i] >= 97 && s[i] <= 122))
-				s[i] -= 32;
-
-			j++;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
 
 		i++;
