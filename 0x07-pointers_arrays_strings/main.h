4806b10a9328096d33fdcae22cd58924b587ae6c
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-26 08:44:20 -0700

    fill memory with constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc - Header file containing function declarations used
+ * in this directory
+ */
+
+int _putchar(int c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
