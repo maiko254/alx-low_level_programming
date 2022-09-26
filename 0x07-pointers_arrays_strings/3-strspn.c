955a9e297ce7d717ba07ee424a5038164f280c15
Michael Bonyo <bonyomichael406@gmail.com>
2022-09-26 09:22:38 -0700

    correction 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,24 +10,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	usigned int bytes = 0;
-	int i;
+	unsigned int a = 0, b, t = 0;
 
-	while (*s)
+	while (accept[a])
 	{
-		for (i = 0; accept[i]; i++)
+		b = 0;
+
+		while (s[b] != 32)
 		{
-			if (*s == accept[i])
-			{
-				bytes++;
-				break;
-			}
-			else if (accept[i + 1] == '\0')
-				return (bytes);
+			if (accept[a] == s[b])
+				t++;
+
+			b++;
 		}
 
-		s++;
+		a++;
 	}
 
-	return (bytes);
+	return (t);
 }
