840ed7af168f7ca52236f02d6bb4d714c2ca7eb0
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-10 22:32:42 -0700

    defining struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Task 0 - Define a new type struct dog with the following elements: name,            type = char *; age, type = float; owner, type = char *;
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - type for dog details
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Type to hold details of name, age and owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
