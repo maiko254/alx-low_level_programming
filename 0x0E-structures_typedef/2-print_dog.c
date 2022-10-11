14b43276885c7bc13d58708087076e6b2370996a
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-11 04:29:49 -0700

    correction Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 
 	if (d->owner != NULL)
-		printf("Owner = %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nil)\n");
 }
