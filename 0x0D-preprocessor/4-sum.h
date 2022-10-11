7279df05f224c8a2148cfb83b75cd1945d71a21e
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-11 09:37:46 -0700

    correction task 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,6 +4,9 @@
 /**
  * Desc: Header file defining a function-like macro SUM(x, y)
  *      that calculates the sum of numbers x and y.
+ *
+ *  @x: first number
+ *  @y: second number
  */
 
 #define SUM(x, y) ((x) + (y))
