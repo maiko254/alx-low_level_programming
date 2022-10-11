940feacc8f4d7882a14ce89639c7d56ed22f0b45
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-11 07:18:41 -0700

    define PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File: 1-p1.h
+ * Author: Michael Bonyo
+ * Desc: Header file that defines a macro named PI
+ *      as abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
