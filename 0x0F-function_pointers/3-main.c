bcb16503e84a1955cf6c9c892c06da7153eb8258
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-13 09:57:46 -0700

    correction Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,10 +10,9 @@
  * Return: Always 0
  */
 
-int main(__attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,23 +20,15 @@ int main(__attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (!op)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
