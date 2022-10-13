016eec195512107ca4cc2cdd65d73f39f033fb9e
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-13 05:09:30 -0700

    parameter function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function given as a parameter on
+ * each element of an array
+ * @array: array of pointer functions
+ * @size: size of the array
+ * @action: pointer to function to use
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,3 @@
 Task 0 - 0-print_name.c - Function that prints a name.
+
+Task 1 - 1-array_iterator.c - Function that executes a function given as a parameter on each element of an array.
