03ada672d45f2b487192f11ab30e3efc2ec8581a
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-14 04:08:02 -0700

    sum of all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 
 	va_start(ap, n);
 
-	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
