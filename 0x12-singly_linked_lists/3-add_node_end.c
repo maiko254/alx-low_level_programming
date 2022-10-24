458205a18e2703db4c97d2f3914036fc3465c6e6
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-24 13:44:40 -0700

    add new node at end of a list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the head of list_t list
+ * @str: string to add to list_t list
+ *
+ * Return: address of the new element or NULL if function fails
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int len;
+	char *d;
+	list_t *new, *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	d = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = d;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+
+	return (*head);
+}
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -5,3 +5,5 @@ Task 0: Print list - Write a function that prints all the elements of a list_t l
 Task 1: List length - Write a function that returns the number of elements in a linked list_t list.
 
 Task 2: Add node - Write a function that adds a new node at the beginning of a list_t list.
+
+Task 3: Add node at the end - Write a function that adds a new node at the end of a list_t list.
