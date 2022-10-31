44844ec5bf584abe69f951621b9316361ba55080
Michael Bonyo <bonyomichael406@gmail.com>
2022-10-31 08:55:05 -0700

    print binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary string to convert
+ *
+ * Return: the converted number or 0 if b is NULL or one or more
+ * chars in string b is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int n = 0, m = 1;
+
+	if (b == '\0')
+		return (0);
+
+	for (i = 0; b[i];)
+		i++;
+
+	for (i -= 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		n += (b[i] - '0') * m;
+		m *= 2;
+	}
+
+	return (n);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,10 @@
+### C - Bit manipulation
+
+Task 0: 0-binary_to_uint - Write a function that converts a binary number to an `unsigned int`.  
+* Prototype: `unsigned int binary_to_uint(const char *b);`
+* where `b` is pointing to a string of `0` and `1` chars
+* Return: the converted number, or 0 if 
+  * there is one or more chars in the string b that is not `0` or `1`
+  * `b` is `NULL`
+
+Task 1: 1-print_binary - Write a function that prints the binary representation of a number.
