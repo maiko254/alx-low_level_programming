b3e98376a7161dd67f730f9fac366c53ed861147
Michael Bonyo <bonyomichael406@gmail.com>
2022-12-08 08:44:21 -0800

    insert node at any position in a doubly linked list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index -  inserts a new node at a given position
+ * @h: pointer to head o list
+ * @idx: index of list to insert node
+ * @n: integer value to add to new node
+ *
+ * Return: NuLL - if function fails
+ *         the address of the new node - otherwise
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *t = *h;
+	unsigned int i;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (t == NULL || t->next == NULL)
+			return (NULL);
+		t = t->next;
+	}
+
+	new->next = t->next;
+	new->prev = t;
+	t->next->prev = new;
+	t->next = new;
+
+	return (new);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -33,3 +33,11 @@
 * 6-sum_dlistint.c - Write a function that returns the sum of all the data (n) of a `dlistint_t` linked list.
    * Prototype: `int sum_dlistint(dlistint_t *head);`
    * if the list is empty, return `0`
+
+### Task 6
+* 7-insert_dnodeint.c - Write a function that inserts a new node at a given position.
+   * Prototype: `dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);`
+   * where `idx` is the index of the list where the new node should be added. Index starts at `0`
+   * Returns: the address of the new node, or `NULL` if it failed
+   * if it is not possible to add the new node at index `idx`, do not add the new node and return `NULL`
+* Your files `2-add_dnodeint.c` and `3-add_dnodeint_end.c` will be compiled during the correction
