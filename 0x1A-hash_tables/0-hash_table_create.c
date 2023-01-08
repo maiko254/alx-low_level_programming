55776762469617857f9a403fe0d304412751ef6c
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-08 02:27:53 -0800

    correction Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table =  malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
