9a7e38057f00be20061b50d3b0f9e408e7abe62c
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-08 02:38:53 -0800

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
 		return (NULL);
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);
