de3e6da0dab5f2c8aa3a94a7176a07e00313a9bc
maiko254 <bonyomichael406@gmail.com>
2023-10-27 05:19:07 -0700

    betty correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,9 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask;
 
 	if (index >= sizeof(n) * 8)
-		return -1;
+		return (-1);
 
 	mask = 1UL << index;
 
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 }
