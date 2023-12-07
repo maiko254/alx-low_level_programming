3e59a3271a6a974829514e64b028294c36c3ee19
maiko254 <bonyomichael406@gmail.com>
2023-12-07 13:55:43 +0000

    free memory allocated to nodes in a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees memory allocated to a doubly linke list
+ * @head: pointer to the head of a doubly linked list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{	
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
