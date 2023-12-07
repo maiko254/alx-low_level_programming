8b8b9b4d866d106f72dd8e6bf0225b7893718ccb
maiko254 <bonyomichael406@gmail.com>
2023-12-07 10:29:37 +0000

    added func description

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,14 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints elements of a doubly-linked list
+ * @h: pointer to head of the linked list
+ * Return: number of nodes in the linked list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp;
-	size_t count = 0;
+	siize_t count = 0;
 
 	if (h == NULL)
 		return (0);
