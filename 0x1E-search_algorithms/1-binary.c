00869b3da0cb392628b551360cd0b5f8ad77c50c
maiko254 <bonyomichael406.com>
2024-04-09 21:06:10 +0300

    Function that searches through an array using binary search algorithm and prints the array being searched every time it changes

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+/**
+ * binary_search - Searches for a value in a sorted array using binary
+ *                 search algorithm
+ * @array: Pointer to the first element in the sorted array to search in
+ * @size: Number of elements in the array
+ * @value: The value to search for in the array
+ * Return: The index where the value is located in the array
+ *         -1 if the value is not found or the array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t high, low, mid, i;
+
+	if (array == NULL)
+		return (-1);
+	high = size - 1;
+	low = 0;
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(",");
+		}
+		printf("\n");
+		mid = (high + low) / 2;
+		if (value > array[mid])
+		{
+			low = mid + 1;
+		}
+		else if (value < array[mid])
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			return (mid);
+		}
+	}
+	return (-1);
+}
