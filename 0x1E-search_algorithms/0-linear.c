17dde0ab047f2d24f4ccadbe26feb75921dfb382
maiko254 <bonyomichael406.com>
2024-04-09 13:36:42 +0300

    Correction: Betty linter errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,16 +13,15 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-		return(-1);
-	for (i = 0; i < size - 1; i++)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			return(i);
-		} else {
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
 		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
-	return(-1);
+	return (-1);
 }
